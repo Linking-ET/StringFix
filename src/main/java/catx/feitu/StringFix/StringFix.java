cc11d1fe24797382d484264374ad02366a870c89
XChen446 <3088576338@qq.com>
2025-08-19 21:35:34 +0800

    Update StringFix.java

--- src/main/java/catx/feitu/StringFix/StringFix.java
+++ src/main/java/catx/feitu/StringFix/StringFix.java
@@ -7,7 +7,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFromToEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class StringFix extends JavaPlugin {
+public final class StringFix extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
